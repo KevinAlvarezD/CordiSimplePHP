3f3b831753e37079c1ad3afd9518392f9910ab29
KevinAlvarezD <kevinadozanam23@gmail.com>
2024-11-01 07:58:02 -0500

    Feat: Create EventRequest

--- app/Http/Requests/EventRequest.php
+++ app/Http/Requests/EventRequest.php
@@ -6,17 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class EventRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        // En esta parte podremos dar autorización a un usuario determinado
         return true;
     }
 
     /**
-     * Get the validation rules that apply to the request.
+     * 
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
